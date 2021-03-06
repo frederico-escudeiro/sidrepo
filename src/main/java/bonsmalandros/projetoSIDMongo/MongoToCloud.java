40cdf9b8d84de424583a84ecaf42bd13f124e6c4
alex-cancelinha <78809571+alex-cancelinha@users.noreply.github.com>
2021-03-06 16:57:47 +0000

    Teste1

---
+++ src/main/java/bonsmalandros/projetoSIDMongo/MongoToCloud.java
@@ -0,0 +1,29 @@
+package bonsmalandros.projetoSIDMongo;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MongoToCloud implements MqttCallback {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    @Override
+    public void connectionLost(Throwable throwable) {
+
+    }
+
+    @Override
+    public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+    }
+}
