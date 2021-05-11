a39b8eb451e247baf32acb78e0ef602314582dec
adrts-iscte <adrts@iscte-iul.pt>
2021-05-11 18:37:30 +0100

    SQLDatabaseConnection corrigido para listar apenas as culturas válidas

--- src/main/java/SQLInitialization/SQLDatabaseConnection.java
+++ src/main/java/SQLInitialization/SQLDatabaseConnection.java
@@ -260,7 +260,7 @@ public class SQLDatabaseConnection {
 
             //criar procedimento que lista culturas
             String dropProcedimentoListarCulturas = "DROP PROCEDURE IF EXISTS `listar_culturas`";
-            String createUtilizadorProcedureListarCulturas = "CREATE DEFINER=`root`@`localhost` PROCEDURE `listar_culturas`() NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER BEGIN SELECT * FROM cultura INNER JOIN utilizador ON cultura.idUtilizador=utilizador.idUtilizador WHERE utilizador.email = (select substring_index(user(),'@localhost', 1)); END";
+            String createUtilizadorProcedureListarCulturas = "CREATE DEFINER=`root`@`localhost` PROCEDURE `listar_culturas`() NOT DETERMINISTIC NO SQL SQL SECURITY DEFINER BEGIN SELECT * FROM cultura INNER JOIN utilizador ON cultura.idUtilizador=utilizador.idUtilizador WHERE utilizador.email = (select substring_index(user(),'@localhost', 1)) and cultura.isValido=1; END";
             statementLocalhost.executeUpdate(dropProcedimentoListarCulturas);
             statementLocalhost.executeUpdate(createUtilizadorProcedureListarCulturas);
 
@@ -529,10 +529,22 @@ public class SQLDatabaseConnection {
             String createInvestigador = "CREATE ROLE investigador;";
             String privilegiosProcedureInvestigador = "GRANT EXECUTE ON PROCEDURE sid2021.alterar_cultura TO 'investigador'";
             String privilegiosUserProcedureInvestigador = "GRANT EXECUTE ON PROCEDURE sid2021.alterar_utilizador TO 'investigador'";
+            String privilegiosAtualizarAlertasProcedureInvestigador = "GRANT EXECUTE ON PROCEDURE sid2021.atualizar_alertas TO 'investigador'";
+            String privilegiosListarAlertasProcedureInvestigador = "GRANT EXECUTE ON PROCEDURE sid2021.listar_alertas TO 'investigador'";
+            String privilegiosListarCulturasProcedureInvestigador = "GRANT EXECUTE ON PROCEDURE sid2021.listar_culturas TO 'investigador'";
+            String privilegiosListarMedicoesProcedureInvestigador = "GRANT EXECUTE ON PROCEDURE sid2021.listar_medicoes TO 'investigador'";
+            String privilegiosMostraCulturaProcedureInvestigador = "GRANT EXECUTE ON PROCEDURE sid2021.mostra_cultura TO 'investigador'";
+            String privilegiosMostraUtilizadorProcedureInvestigador = "GRANT EXECUTE ON PROCEDURE sid2021.mostra_utilizador TO 'investigador'";
             statementLocalhost.executeUpdate(dropRoleInvestigador);
             statementLocalhost.executeUpdate(createInvestigador);
             statementLocalhost.executeUpdate(privilegiosProcedureInvestigador);
             statementLocalhost.executeUpdate(privilegiosUserProcedureInvestigador);
+            statementLocalhost.executeUpdate(privilegiosAtualizarAlertasProcedureInvestigador);
+            statementLocalhost.executeUpdate(privilegiosListarAlertasProcedureInvestigador);
+            statementLocalhost.executeUpdate(privilegiosListarCulturasProcedureInvestigador);
+            statementLocalhost.executeUpdate(privilegiosListarMedicoesProcedureInvestigador);
+            statementLocalhost.executeUpdate(privilegiosMostraCulturaProcedureInvestigador);
+            statementLocalhost.executeUpdate(privilegiosMostraUtilizadorProcedureInvestigador);
 
             //Criar ROLE administrador
             String dropRoleAdmin = "DROP ROLE IF EXISTS `administrador`;";
@@ -547,6 +559,12 @@ public class SQLDatabaseConnection {
             String privilegiosCriarUtilizadorProcedureAdministrador = "GRANT EXECUTE ON PROCEDURE sid2021.criar_utilizador TO 'administrador'";
             String privilegiosRemoverUtilizadorProcedureAdministrador = "GRANT EXECUTE ON PROCEDURE sid2021.remover_utilizador TO 'administrador'";
             String privilegiosAlterarUtilizadorProcedureAdministrador = "GRANT EXECUTE ON PROCEDURE sid2021.alterar_utilizador TO 'administrador'";
+            String privilegiosAtualizarAlertasProcedureAdministrador = "GRANT EXECUTE ON PROCEDURE sid2021.atualizar_alertas TO 'administrador'";
+            String privilegiosListarAlertasProcedureAdministrador = "GRANT EXECUTE ON PROCEDURE sid2021.listar_alertas TO 'administrador'";
+            String privilegiosListarCulturasProcedureAdministrador = "GRANT EXECUTE ON PROCEDURE sid2021.listar_culturas TO 'administrador'";
+            String privilegiosListarMedicoesProcedureAdministrador = "GRANT EXECUTE ON PROCEDURE sid2021.listar_medicoes TO 'administrador'";
+            String privilegiosMostraCulturaProcedureAdministrador = "GRANT EXECUTE ON PROCEDURE sid2021.mostra_cultura TO 'administrador'";
+            String privilegiosMostraUtilizadorProcedureAdministrador = "GRANT EXECUTE ON PROCEDURE sid2021.mostra_utilizador TO 'administrador'";
             statementLocalhost.executeUpdate(dropRoleAdmin);
             statementLocalhost.executeUpdate(createAdmin);
             statementLocalhost.executeUpdate(privilegiosSelectCulturaAdministrador);
@@ -559,6 +577,12 @@ public class SQLDatabaseConnection {
             statementLocalhost.executeUpdate(privilegiosCriarUtilizadorProcedureAdministrador);
             statementLocalhost.executeUpdate(privilegiosRemoverUtilizadorProcedureAdministrador);
             statementLocalhost.executeUpdate(privilegiosAlterarUtilizadorProcedureAdministrador);
+            statementLocalhost.executeUpdate(privilegiosAtualizarAlertasProcedureAdministrador);
+            statementLocalhost.executeUpdate(privilegiosListarAlertasProcedureAdministrador);
+            statementLocalhost.executeUpdate(privilegiosListarCulturasProcedureAdministrador);
+            statementLocalhost.executeUpdate(privilegiosListarMedicoesProcedureAdministrador);
+            statementLocalhost.executeUpdate(privilegiosMostraCulturaProcedureAdministrador);
+            statementLocalhost.executeUpdate(privilegiosMostraUtilizadorProcedureAdministrador);
 
             //Criar ROLE técnico de manutenção
             String dropRoleTecnico = "DROP ROLE IF EXISTS `tecnico`;";
